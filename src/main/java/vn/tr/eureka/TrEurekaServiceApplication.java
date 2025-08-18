1cb0bb87fc9f6c312a54d031e37e56a6cfa22848
tyran8x <tyran8x@gmail.com>
2025-08-18 10:52:43 +0700

    update

--- src/main/java/vn/tr/eureka/TrEurekaServiceApplication.java
+++ src/main/java/vn/tr/eureka/TrEurekaServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServiceApplication {
-
+public class TrEurekaServiceApplication {
+	
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServiceApplication.class, args);
+		SpringApplication.run(TrEurekaServiceApplication.class, args);
 	}
-
+	
 }
